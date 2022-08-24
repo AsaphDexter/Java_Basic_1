9c99bf2d14dad729f976658aaa307f0956e27e79
AsaphDexter <36012956+AsaphDexter@users.noreply.github.com>
2022-08-24 17:49:31 +0100

    Add files via upload

---
+++ JBE_1.java
@@ -0,0 +1,16 @@
+package jbe_1;
+/**
+ *
+ * @author AsaphDexter
+ * 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line. Go to the editor
+Expected Output :
+Hello
+*Alexandra Abramov
+ */
+public class JBE_1 {
+
+    public static void main(String[] args) {
+       System.out.println("Hello");
+       System.out.println("Alexandra Abramov" );
+    }
+}
